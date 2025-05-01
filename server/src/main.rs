8182f841585a4def6c91a551250957062bbf9d83
NineSuper <NineSuper66@gmail.com>
2025-05-02 00:53:03 +0200

    update main.rs

--- server/src/main.rs
+++ server/src/main.rs
@@ -6,13 +6,20 @@
 /*   By: tde-los- <tde-los-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/29 15:23:47 by tde-los-          #+#    #+#             */
-/*   Updated: 2025/04/30 16:25:55 by tde-los-         ###   ########.fr       */
+/*   Updated: 2025/05/01 23:07:36 by tde-los-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 mod init_env;
+mod server_state;
+mod map;
+
+use server_state::ServerConfig;
 
 fn	main()
 {
-	init_env::init_env();
+	let config: ServerConfig = init_env::init_env();
+	let map: Vec<Vec<map::Cell>> = map::create_map(config.width, config.height);
+
+    println!("{:?}", map[0][0]);
 }
