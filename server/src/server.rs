f95d586ccfc070874b1c1c672807064ea94a2236
NineSuper <NineSuper66@gmail.com>
2025-05-06 19:24:55 +0200

    Ajout de l'initialisation des sockets lors de l'ouverture du serveur

--- server/src/server.rs
+++ server/src/server.rs
@@ -6,13 +6,14 @@
 /*   By: tde-los- <tde-los-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/01 22:12:16 by tde-los-          #+#    #+#             */
-/*   Updated: 2025/05/06 18:51:19 by tde-los-         ###   ########.fr       */
+/*   Updated: 2025/05/06 19:24:35 by tde-los-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 use std::net::{TcpListener, TcpStream};
 use std::process::exit;
 use std::collections::HashMap;
+use colored::*;
 
 #[derive(Debug, Clone)]
 pub struct ServerConfig
@@ -20,7 +21,7 @@ pub struct ServerConfig
     pub _port: u32,
     pub width: u32,
     pub height: u32,
-    pub _clients: u32,
+    pub _connexion_max: u32,
     pub _time_unit: u32,
     pub teams: Vec<String>,
 }
@@ -33,7 +34,7 @@ fn	setup_listener(addr: &String) -> TcpListener
     {
         Ok(listener) => {
 			// listener.set_nonblocking(true);
-            println!("✅ Serveur ouvert sur: {}", addr);
+            println!("✅ Serveur ouvert sur: {}\n", addr);
             return listener;
         }
         Err(e) => {
@@ -43,17 +44,21 @@ fn	setup_listener(addr: &String) -> TcpListener
     }
 }
 
-fn	handle_client()
+fn	handle_client(clients: &mut HashMap<i32, TcpStream>)
 {
-
+	for (id, stream) in clients.iter()
+	{
+		println!("[DEBUG] {id} {stream:?}");
+	}
 }
 
-fn	accept_new_clients(listener: &TcpListener)
+fn	accept_new_client(clients: &mut HashMap<i32, TcpStream>, listener: &TcpListener, next_id: &mut i32)
 {
-	match listener.accept()
+	if let Ok((stream, addr)) = listener.accept()
 	{
-		Ok((_socket, addr)) => println!("[DEBUG] Nouveau client: {addr:?}"),
-		Err(e) => println!("couldn't get client: {e:?}"),
+		println!("{}{addr:?}", "Nouveau client: ".green());
+		clients.insert(*next_id, stream);
+		*next_id += 1;
 	}
 }
 
@@ -62,10 +67,11 @@ pub fn	init_server(config: ServerConfig)
 	let	addr = format!("127.0.0.1:{}", config._port);
 	let	listener = setup_listener(&addr);
 	let mut	clients: HashMap<i32, TcpStream> = HashMap::new();
+	let mut next_id = 0;
 
 	loop
 	{
-		accept_new_clients(&listener);
-		handle_client();
+		accept_new_client(&mut clients, &listener, &mut next_id);
+		handle_client(&mut clients);
 	}
 }
